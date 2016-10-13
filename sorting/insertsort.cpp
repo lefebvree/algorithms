5c56442e9f29052e9a8dbcef052f1970dea573d3
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-10-13 13:33:23 +0200

    Update insertsort.cpp

--- sorting/insertsort.cpp
+++ sorting/insertsort.cpp
@@ -13,7 +13,7 @@ using namespace std;
  *  @var
  *    selected ->  indice of the selected element
  */
-void insertSort ( int t[], int n ) {
+void insertsort ( int t[], int n ) {
     int selected;
 
     for (int i = 1; i < n; i++) {
