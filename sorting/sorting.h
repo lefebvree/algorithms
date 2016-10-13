659915f48b9c7ff09548a936e3b570a5b0de855c
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-10-13 13:33:08 +0200

    Update sorting.h

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -8,6 +8,9 @@ void bubblesort ( int t[], int n ) ;
 // Select Sort
 void selectsort ( int t[], int n ) ;
 
+// Insert Sort
+void insertsort ( int t[], int n ) ;
+
 // Merge Sort
 void mergeSort  ( int t[], int i, int j ) ;
 void merge      ( int t[], int i, int j, int k );
