3e4946ce98152955dda7876fcb51758c14c2a610
Etienne Lefebvre <etiennelefebvre@outlook.com>
2016-10-13 14:07:13 +0200

    Created main test program

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -2,17 +2,20 @@
 #ifndef SORT_H
 #define SORT_H
 
+// Random array creation
+void randomArray ( int t[], int n, int min, int max );
+
 // Bubble Sort
-void bubbleSort ( int t[], int n ) ;
+void bubbleSort ( int t[], int n );
 
 // Select Sort
-void selectSort ( int t[], int n ) ;
+void selectSort ( int t[], int n );
 
 // Insert Sort
-void insertSort ( int t[], int n ) ;
+void insertSort ( int t[], int n );
 
 // Merge Sort
-void mergeSort  ( int t[], int i, int j ) ;
+void mergeSort  ( int t[], int i, int j );
 void merge      ( int t[], int i, int j, int k );
 
 // Quick Sort
