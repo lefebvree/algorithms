63eb33c20a2e6ca9254781e384113ee1b0d920ef
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-10-13 13:03:21 +0200

    Update bubblesort.cpp

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 /*
  * Look into 't' and swap t[i] and t[i+1] if t[i] is uper than t[i+1]
+ *  @param
+ *      t[] -> array to sort
+ *      n   -> size of 't'
  *
- *  t[] -> array to sort
- *  n   -> size of 't'
+ *  @var
+ *      swapped ->  if two elements are swapped or not
  */
 void bubblesort ( int t[], int n ) {
     bool swapped = true;
