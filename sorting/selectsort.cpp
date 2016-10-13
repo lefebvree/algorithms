33efc3ea75da973b118460eebad36b9b3ed12556
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-10-13 13:02:17 +0200

    add selectsort.cpp

---
+++ sorting/selectsort.cpp
@@ -0,0 +1,29 @@
+#include <utility>
+
+#include "sorting.h"
+
+using namespace std;
+
+/*
+ * Save the first element as minimum and check if there is a lower element
+ *  @param
+ *    t[] ->  array to sort
+ *    n   ->  size of 't'
+ *
+ *  @var
+ *    min ->  minimum value of unsorted element
+ */
+void selectSort ( int t[], int n ) {
+    int min;
+
+    for (int i = 0; i < n - 1; i++) {
+        min = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (t[j] < t[min]) {
+                min = j;
+            }
+        }
+        swap(t[i], t[min]);
+    }
+}
