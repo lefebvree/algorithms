85ead36b3d5fb47c8c53f00899b39dac47d0de45
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-10-13 13:32:28 +0200

    Create insertsort.cpp

---
+++ sorting/insertsort.cpp
@@ -0,0 +1,29 @@
+#include <utility>
+
+#include "sorting.h"
+
+using namespace std;
+
+/*
+ * Extract the first of unsorted elements and compare with previous values to insert it at the right place
+ *  @param
+ *    t[] ->  array to sort
+ *    n   ->  size of 't'
+ *
+ *  @var
+ *    selected ->  indice of the selected element
+ */
+void insertSort ( int t[], int n ) {
+    int selected;
+
+    for (int i = 1; i < n; i++) {
+        selected = i;
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (t[selected] < t[j]) {
+                swap(t[selected], t[j]);
+                selected--;
+            }
+        }
+    }
+}
